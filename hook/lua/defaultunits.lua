7adffeb8c09d6d2d0831203e2063e9461207a83f
Exotic-Retard <ddoogedoge@gmail.com>
2015-11-30 19:03:45 +0000

    More changes from range rework
    1. acu now gets same exp form all tech levels again
    2. aeon lab script added, was not going to work otherwise : (
    3. scouts fixed/tweaked
    4.*Ilshavoh* typo fixed and turret turn rate fixed from brokeness
    5. all acus now have more turret turn rate so you can micro better.

--- hook/lua/defaultunits.lua
+++ hook/lua/defaultunits.lua
@@ -1,29 +1,3 @@
-local oldACUUnit = ACUUnit
-ACUUnit = Class(oldACUUnit) {
-
-    -- Called when this unit kills another. Chiefly responsible for the veterancy system for now.
-    OnKilledUnit = function(self, unitKilled, massKilled)
-        if not massKilled then return end -- Make sure engine calls aren't passed with massKilled == 0
-        
-        if not IsAlly(self:GetArmy(), unitKilled:GetArmy()) then
-        
-            -- Adjust mass based on unit killed for ACU
-            local techIndex = function()
-                for k, tech in pairs({'TECH1', 'TECH2', 'TECH3', 'EXPERIMENTAL', 'COMMAND'}) do
-                    if tech == unitKilled.techLevel then return k end
-                end
-            end
-            massKilled = massKilled / techIndex()
-            
-            if unitKilled.Sync.VeteranLevel then
-                massKilled = massKilled * math.max((unitKilled.Sync.VeteranLevel - self.Sync.VeteranLevel), 1)
-            end
-
-            self:CalculateVeterancyLevel(massKilled) -- Bails if we've not gone up
-        end
-    end,
-}
-    
 local oldAirUnit = AirUnit
 AirUnit = Class(oldAirUnit) {
     --- Called when the unit is killed, but before it falls out of the sky and blows up.
