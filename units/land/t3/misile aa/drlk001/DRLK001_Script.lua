26ece218660d9d69110b6d67c541100491e0f1a0
Ithilis <fhobos@azet.sk>
2015-12-29 01:03:54 +0100

    bouncer remove targeting lasser +tmd +0,5r

--- units/land/t3/misile aa/drlk001/DRLK001_Script.lua
+++ units/land/t3/misile aa/drlk001/DRLK001_Script.lua
@@ -10,24 +10,15 @@
 local CWalkingLandUnit = import('/lua/cybranunits.lua').CWalkingLandUnit
 local CybranWeaponsFile = import('/lua/cybranweapons.lua')
 local CAANanoDartWeapon = CybranWeaponsFile.CAANanoDartWeapon
-local TargetingLaser = import('/lua/kirvesweapons.lua').TargetingLaser
 local EffectUtils = import('/lua/effectutilities.lua')
 local Effects = import('/lua/effecttemplates.lua')
 
 DRLK001 = Class(CWalkingLandUnit) 
 {
     Weapons = {
-        AAGun = Class(CAANanoDartWeapon) {},    
-        Lazor = Class(TargetingLaser) {
-            FxMuzzleFlash = {'/effects/emitters/particle_cannon_muzzle_02_emit.bp'},
-        },
+        AAGun = Class(CAANanoDartWeapon) {},
         GroundGun = Class(CAANanoDartWeapon) {},
     },
-    
-    OnKilled = function(self, instigator, type, overkillRatio)
-        self:SetWeaponEnabledByLabel('Lazor', false)
-        CWalkingLandUnit.OnKilled(self, instigator, type, overkillRatio)
-    end,
     
 }
 
