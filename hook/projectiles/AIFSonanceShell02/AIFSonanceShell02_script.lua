e63dc175d5280e255d7234cdcc87b5587ec5924b
Exotic-Retard <ddoogedoge@gmail.com>
2016-02-01 15:38:59 +0000

    Aeon T3 arty cosmetic projectile change
    the old effect was ugly and didnt show its damage over time nature so it
    now has the t2 aeon arty green burning effect, which now also works on
    water.

---
+++ hook/projectiles/AIFSonanceShell02/AIFSonanceShell02_script.lua
@@ -0,0 +1,27 @@
+#
+# Aeon Artillery Projectile
+#
+local AArtilleryProjectile = import('/lua/aeonprojectiles.lua').AArtilleryProjectile
+local EffectTemplate = import('/lua/EffectTemplates.lua')
+local RandomFloat = import('/lua/utilities.lua').GetRandomFloat
+
+AIFSonanceShell02 = Class(AArtilleryProjectile) {
+    
+    FxTrails = EffectTemplate.ASonanceWeaponFXTrail02,
+    
+    FxImpactUnit =  EffectTemplate.AMiasmaField01,
+    FxImpactProp =  EffectTemplate.AMiasmaField01,
+    FxImpactLand =  EffectTemplate.AMiasmaField01,
+    FxImpactNone =  EffectTemplate.AMiasmaField01,
+    
+    OnImpact = function(self, TargetType, targetEntity)
+        local rotation = RandomFloat(0,2*math.pi)
+        
+        CreateDecal(self:GetPosition(), rotation, 'crater_radial01_normals', '', 'Alpha Normals', 10, 10, 300, 0, self:GetArmy())
+        CreateDecal(self:GetPosition(), rotation, 'crater_radial01_albedo', '', 'Albedo', 12, 12, 300, 0, self:GetArmy())
+ 
+        AArtilleryProjectile.OnImpact( self, TargetType, targetEntity )
+    end,
+}
+
+TypeClass = AIFSonanceShell02
