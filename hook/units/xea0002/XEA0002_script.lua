26d2f2b55b093000121aee94e7874dd183d7a93d
Ithilis <fhobos@azet.sk>
2016-03-02 13:51:31 +0100

    Novax jamming
    Its not done..  spend on it 3 horse for now, but it need multiple fix :(
    - satelite dont drain energy, this is bug also in normal game
    - it create one land and one air units that have jamming itself, but
    sera crystal create multiple units, i copypast but it dont work same
    dont know why.
    - log is full of some shit that i dont understand what want to mean..
    - potencial problem with vet mod, i setup jaming unti to be targetable
    and destroyable, maybe it should create problems somehow..
    - jamming toggle dont work, probably becasue it create another units,
    probably will fix it some function that say if jaming toggle off then
    fork function is off also..
    
    - changelog is missing while its not finish

---
+++ hook/units/xea0002/XEA0002_script.lua
@@ -0,0 +1,120 @@
+#****************************************************************************
+#**
+#**  File     :  /cdimage/units/XEA0002/XEA0002_script.lua
+#**  Author(s):  Drew Staltman, Gordon Duclos
+#**
+#**  Summary  :  UEF Defense Satelite Script
+#**
+#**  Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+local TAirUnit = import('/lua/terranunits.lua').TAirUnit
+local TOrbitalDeathLaserBeamWeapon = import('/lua/terranweapons.lua').TOrbitalDeathLaserBeamWeapon
+local VizMarker = import('/lua/sim/VizMarker.lua').VizMarker
+
+XEA0002 = Class(TAirUnit) {
+    DestroyNoFallRandomChance = 1.1,
+    
+    HideBones = { 'Shell01', 'Shell02', 'Shell03', 'Shell04', },
+    
+    Weapons = {
+        OrbitalDeathLaserWeapon = Class(TOrbitalDeathLaserBeamWeapon){},
+    },
+    
+    OnCreate = function(self, builder, layer)
+        self:ForkThread(self.LandBlipThread)
+        self:ForkThread(self.AirBlipThread)
+    end,
+    
+    LandBlipThread = function(self)
+        local position = self:GetPosition()
+
+        while not self:IsDead() do
+            --Spawn land blips
+            self.landChildUnit = CreateUnitHPR('XSC9010', self:GetArmy(), position[1], position[2], position[3], 0, 0, 0)
+            self.landChildUnit.parentCrystal = self
+            
+            IssuePatrol({self.landChildUnit}, {position[1] + Random(-10, 10), position[2], position[3] + Random(-10, 10)})
+            IssuePatrol({self.landChildUnit}, {position[1] + Random(-10, 10), position[2], position[3] + Random(-10, 10)})
+            IssuePatrol({self.landChildUnit}, {position[1] + Random(-10, 10), position[2], position[3] + Random(-10, 10)})
+            IssuePatrol({self.landChildUnit}, {position[1] + Random(-10, 10), position[2], position[3] + Random(-10, 10)})
+
+            WaitSeconds(Random(7,13))
+
+            self.landChildUnit:Destroy()
+            self.landChildUnit = nil
+
+            #WaitSeconds(Random(2,7))
+        end
+    end,
+
+    AirBlipThread = function(self)
+        local position = self:GetPosition()
+
+        while not self:IsDead() do
+            --Spawn air blips
+            self.airChildUnit = CreateUnitHPR('XSC9011', self:GetArmy(), position[1], position[2], position[3], 0, 0, 0)
+            self.airChildUnit.parentCrystal = self
+
+            IssuePatrol({self.airChildUnit}, {position[1] + Random(-10, 10), position[2], position[3] + Random(-10, 10)})
+            IssuePatrol({self.airChildUnit}, {position[1] + Random(-10, 10), position[2], position[3] + Random(-10, 10)})
+            IssuePatrol({self.airChildUnit}, {position[1] + Random(-10, 10), position[2], position[3] + Random(-10, 10)})
+            IssuePatrol({self.airChildUnit}, {position[1] + Random(-10, 10), position[2], position[3] + Random(-10, 10)})
+
+            WaitSeconds(Random(7,13))
+
+            self.airChildUnit:Destroy()
+            self.airChildUnit = nil
+
+            #WaitSeconds(Random(2,7))
+        end
+    end,
+    
+    
+    
+    
+    OnKilled = function(self, instigator, type, overkillRatio)
+        if self.IsDying then 
+            return 
+        end
+        
+        local wep = self:GetWeaponByLabel('OrbitalDeathLaserWeapon')
+        for k, v in wep.Beams do
+            v.Beam:Disable()
+        end      
+        
+        self.IsDying = true
+        self.Parent:Kill(instigator, type, 0)
+        
+        TAirUnit.OnKilled(self, instigator, type, overkillRatio)      
+
+        if self.airChildUnit then self.airChildUnit:Destroy() end
+        if self.landChildUnit then self.landChildUnit:Destroy() end        
+    end,
+    
+    Open = function(self)
+        ChangeState( self, self.OpenState )
+    end,
+    
+    OpenState = State() {
+        Main = function(self)
+            self.OpenAnim = CreateAnimator(self)
+            self.OpenAnim:PlayAnim( '/units/XEA0002/xea0002_aopen01.sca' )
+            self.Trash:Add( self.OpenAnim )
+            WaitFor( self.OpenAnim )
+            
+            self.OpenAnim:PlayAnim( '/units/XEA0002/xea0002_aopen02.sca' )
+            
+            for k,v in self.HideBones do
+                self:HideBone( v, true )
+            end
+        end,
+    },
+    
+    --Make this unit invulnerable - Not any more! (IceDreamer)
+    OnDamage = function(self, instigator, amount, vector, damageType)
+		if EntityCategoryContains(categories.NUKE, instigator) then
+			self:Destroy()
+		end
+    end,
+}
+TypeClass = XEA0002
