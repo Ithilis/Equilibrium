f4d2e8a9d29601a6f039e04c5e55a06a9cf5f9aa
Ithilis <fhobos@azet.sk>
2016-05-20 18:33:26 +0200

    cybran: strat + t3 gunship + deciver
    strat decrese surviability and increse e maintaince while stealth is op
    and now one pass t2 mex as other
    t3 gunship instece maintaince for steath
    deciver optimilize code to by merged

--- hook/units/url0306/URL0306_Script.lua
+++ hook/units/url0306/URL0306_Script.lua
@@ -1,50 +1,8 @@
-#****************************************************************************
-#**
-#**  File     :  /cdimage/units/URL0306/URL0306_script.lua
-#**  Author(s):  Jessica St. Croix
-#**
-#**  Summary  :  Cybran Mobile Radar Jammer Script
-#**
-#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
-#****************************************************************************
+--Deciever - mobile stealth generator
 
-local CLandUnit = import('/lua/cybranunits.lua').CLandUnit
-local EffectUtil = import('/lua/EffectUtilities.lua')
-
-URL0306 = Class(CLandUnit) {
-    OnStopBeingBuilt = function(self,builder,layer)
-        CLandUnit.OnStopBeingBuilt(self,builder,layer)
-        self:SetMaintenanceConsumptionActive()
-    end,
-    
-    IntelEffects = {
-		{
-			Bones = {
-				'AttachPoint',
-			},
-			Offset = {
-				0,
-				0.3,
-				0,
-			},
-			Scale = 0.2,
-			Type = 'Jammer01',
-		},
-    },
-    
-    OnIntelEnabled = function(self)
-        CLandUnit.OnIntelEnabled(self)
-        if self.IntelEffects then
-			self.IntelEffectsBag = {}
-			self.CreateTerrainTypeEffects( self, self.IntelEffects, 'FXIdle',  self:GetCurrentLayer(), nil, self.IntelEffectsBag )
-		end
-    end,
-
-    OnIntelDisabled = function(self)
-        CLandUnit.OnIntelDisabled(self)
-        EffectUtil.CleanupEffectBag(self,'IntelEffectsBag')
-    end,    
-    
+local oldURL0306 = URL0306
+URL0306 = Class(oldURL0306) {
+        
     RetardedTurnOffStealth = function(self)
         WARN('turning off stealth')
         self:DisableIntel('RadarStealthField') -- for some absurd reason toggling scriptbit for stealth doesnt work here, but does work OnStopBeingBuilt. crazy. so i just did it manually.
