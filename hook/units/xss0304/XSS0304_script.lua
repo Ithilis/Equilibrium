3a4dce3c20add2eda1ffa6278656ed311ebe9cbc
Ithilis <fhobos@azet.sk>
2016-05-22 00:58:30 +0200

    Revert "sera t3 subhunter test version"
    This reverts commit bf4b79b91e1eba9402d8980726b8a143899cffa1.

--- hook/units/xss0304/XSS0304_script.lua
+++ hook/units/xss0304/XSS0304_script.lua
@@ -3,50 +3,19 @@
 local oldXSS0304 = XSS0304
 XSS0304 = Class(oldXSS0304) {
 
-    OnStopBeingBuilt = function(self, builder, layer)
-        SSubUnit.OnStopBeingBuilt(self, builder, layer)
-        self:DisableUnitIntel('RadarStealth')
-        self:DisableUnitIntel('SonarStealth')
-        self.Cloaked = false
-        ChangeState( self, self.InvisState ) -- If spawned in we want the unit to be invis, normally the unit will immediately start moving
+    OnCreate = function(self)
+        SSubUnit.OnCreate(self)
+        self:SetMaintenanceConsumptionActive()
     end,
     
-    
-    InvisState = State() {
-        Main = function(self)
-            self.Cloaked = false
-            local bp = self:GetBlueprint()
-            if bp.Intel.StealthWaitTime then
-                WaitSeconds( bp.Intel.StealthWaitTime )
-            end
-            self:EnableUnitIntel('RadarStealth')
-            self:EnableUnitIntel('SonarStealth')
-            self.Cloaked = true
-        end,
-        
-        OnMotionHorzEventChange = function(self, new, old)
-            if new != 'Stopped' then
-                ChangeState( self, self.VisibleState )
-            end
-            SSubUnit.OnMotionHorzEventChange(self, new, old)
-        end,
-    },
-    
-    VisibleState = State() {
-        Main = function(self)
-            if self.Cloaked then
-                self:DisableUnitIntel('RadarStealth')
-                self:DisableUnitIntel('SonarStealth')
-            end
-        end,
-        
-        OnMotionHorzEventChange = function(self, new, old)
-            if new == 'Stopped' then
-                ChangeState( self, self.InvisState )
-            end
-            SSubUnit.OnMotionHorzEventChange(self, new, old)
-        end,
-    },
+    OnMotionVertEventChange = function( self, new, old )
+        SSubUnit.OnMotionVertEventChange(self, new, old)
+        if new == 'Top' then
+            self:SetMaintenanceConsumptionInactive()
+        elseif new == 'Down' then
+            self:SetMaintenanceConsumptionActive()
+        end
+    end,
 
 }
 TypeClass = XSS0304
