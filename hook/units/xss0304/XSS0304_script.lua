473253000ac62e3673f201d156f44654a670378c
Ithilis <fhobos@azet.sk>
2016-05-22 00:42:11 +0200

    sera t3 subhunter test version
    less price depch charges, less hp less dmg, stealth on selen style.  not
    sure how will work.

--- hook/units/xss0304/XSS0304_script.lua
+++ hook/units/xss0304/XSS0304_script.lua
@@ -3,19 +3,50 @@
 local oldXSS0304 = XSS0304
 XSS0304 = Class(oldXSS0304) {
 
-    OnCreate = function(self)
-        SSubUnit.OnCreate(self)
-        self:SetMaintenanceConsumptionActive()
+    OnStopBeingBuilt = function(self, builder, layer)
+        SSubUnit.OnStopBeingBuilt(self, builder, layer)
+        self:DisableUnitIntel('RadarStealth')
+        self:DisableUnitIntel('SonarStealth')
+        self.Cloaked = false
+        ChangeState( self, self.InvisState ) -- If spawned in we want the unit to be invis, normally the unit will immediately start moving
     end,
     
-    OnMotionVertEventChange = function( self, new, old )
-        SSubUnit.OnMotionVertEventChange(self, new, old)
-        if new == 'Top' then
-            self:SetMaintenanceConsumptionInactive()
-        elseif new == 'Down' then
-            self:SetMaintenanceConsumptionActive()
-        end
-    end,
+    
+    InvisState = State() {
+        Main = function(self)
+            self.Cloaked = false
+            local bp = self:GetBlueprint()
+            if bp.Intel.StealthWaitTime then
+                WaitSeconds( bp.Intel.StealthWaitTime )
+            end
+            self:EnableUnitIntel('RadarStealth')
+            self:EnableUnitIntel('SonarStealth')
+            self.Cloaked = true
+        end,
+        
+        OnMotionHorzEventChange = function(self, new, old)
+            if new != 'Stopped' then
+                ChangeState( self, self.VisibleState )
+            end
+            SSubUnit.OnMotionHorzEventChange(self, new, old)
+        end,
+    },
+    
+    VisibleState = State() {
+        Main = function(self)
+            if self.Cloaked then
+                self:DisableUnitIntel('RadarStealth')
+                self:DisableUnitIntel('SonarStealth')
+            end
+        end,
+        
+        OnMotionHorzEventChange = function(self, new, old)
+            if new == 'Stopped' then
+                ChangeState( self, self.InvisState )
+            end
+            SSubUnit.OnMotionHorzEventChange(self, new, old)
+        end,
+    },
 
 }
 TypeClass = XSS0304
