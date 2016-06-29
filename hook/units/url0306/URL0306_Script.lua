ca295d3c04ac8d84c186c2ff894cdc7f4f429202
Exotic-Retard <ddoogedoge@gmail.com>
2016-06-29 01:40:54 +0100

    Fix and clean Deceiver code
    now it should not have hardcoded urls, be a little bit cleaner and work
    properly.

--- hook/units/url0306/URL0306_Script.lua
+++ hook/units/url0306/URL0306_Script.lua
@@ -1,12 +1,8 @@
 --Deciever - mobile stealth generator
-local DummyWeapon = import('/mods/Equilibrium/lua/EQweapons.lua').DummyLaser -- custom invisible weapon ; when copying this copy EQweapons and EQbeams! update urls in there as well!
 
 
 local oldURL0306 = URL0306
 URL0306 = Class(oldURL0306) {
-    Weapons = {
-        TargetFinder = Class(DummyWeapon) {} -- needed for the dummy weapon added to stop some distance away
-    },
     
     RetardedTurnOffStealth = function(self)
         self:DisableIntel('RadarStealthField') -- for some absurd reason toggling scriptbit for stealth doesnt work here, but does work OnStopBeingBuilt. crazy. so i just did it manually.
@@ -15,26 +11,29 @@ URL0306 = Class(oldURL0306) {
         EffectUtil.CleanupEffectBag(self,'IntelEffectsBag')
     end,
     
-    RetardedTurnOnStealth = function(self)
+    RetardedTurnOnStealth = function(self) 
+        -- because fuck you thats why
         self:EnableIntel('RadarStealthField')
         self:EnableIntel('RadarStealth') 
         self:SetMaintenanceConsumptionActive()
+        
         if self.IntelEffects then
 			self.IntelEffectsBag = {}
 			self.CreateTerrainTypeEffects( self, self.IntelEffects, 'FXIdle',  self:GetCurrentLayer(), nil, self.IntelEffectsBag )
 		end
-        -- because fuck you thats why
+        
         self:SetScriptBit('RULEUTC_StealthToggle', false)
     end,
-    
     
     OnAttachedToTransport = function(self, transport, bone)
         self:MarkWeaponsOnTransport(true)
         self:RetardedTurnOffStealth() -- stealth off
+        
         if self:ShieldIsOn() then
             self:DisableShield()
             self:DisableDefaultToggleCaps()
         end
+        
         self:DoUnitCallbacks( 'OnAttachedToTransport', transport, bone)
     end,
 
