f9f25c71374c186e831c60b7a13bd71216ff512e
Ithilis <fhobos@azet.sk>
2016-06-29 15:22:54 +0200

    beetle add death weapon
    do 50% of dmg when is destroed

--- hook/units/xrl0302/XRL0302_Script.lua
+++ hook/units/xrl0302/XRL0302_Script.lua
@@ -4,10 +4,10 @@ local oldXRL0302 = XRL0302
 XRL0302 = Class(oldXRL0302) {
 
         OnStopBeingBuilt = function(self,builder,layer)
-        CWalkingLandUnit.OnStopBeingBuilt(self,builder,layer)
-        self:SetMaintenanceConsumptionInactive()
-        self:SetScriptBit('RULEUTC_CloakToggle', true)
-        self:RequestRefreshUI()
+            CWalkingLandUnit.OnStopBeingBuilt(self,builder,layer)
+            self:SetMaintenanceConsumptionInactive()
+            self:SetScriptBit('RULEUTC_CloakToggle', true)
+            self:RequestRefreshUI()
         end
 }
 TypeClass = XRL0302
