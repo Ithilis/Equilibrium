459cd48c760bfc28ec584e447ed9bdf46da21511
Exotic-Retard <ddoogedoge@gmail.com>
2016-06-30 01:13:01 +0100

    Make Firebeetle fx show up in all cases of death
    since we have a death explosion, its only fair to show it when it goes
    off. and it goes off all the time.

--- hook/units/xrl0302/XRL0302_Script.lua
+++ hook/units/xrl0302/XRL0302_Script.lua
@@ -1,13 +1,43 @@
---mobile bomb
+-- File     :  /data/units/XRL0302/XRL0302_script.lua
+-- Author(s):  Jessica St. Croix, Gordon Duclos
+-- Summary  :  Cybran Mobile Bomb Script
+-- Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
+-----------------------------------------------------------------
+local CWalkingLandUnit = import('/lua/cybranunits.lua').CWalkingLandUnit
+local CMobileKamikazeBombWeapon = import('/lua/cybranweapons.lua').CMobileKamikazeBombWeapon
+local EffectTemplate = import('/lua/EffectTemplates.lua')
 
-local oldXRL0302 = XRL0302
-XRL0302 = Class(oldXRL0302) {
+XRL0302 = Class(CWalkingLandUnit) {
 
-        OnStopBeingBuilt = function(self,builder,layer)
-            CWalkingLandUnit.OnStopBeingBuilt(self,builder,layer)
-            self:SetMaintenanceConsumptionInactive()
-            self:SetScriptBit('RULEUTC_CloakToggle', true)
-            self:RequestRefreshUI()
+
+
+    -- OK this unit was horribly overcomplicated in the past. What we want is for only one of 
+    -- three situations to ever blow up the bomb. The unit getting in range and firing, the 
+    -- player pressing the detonate button, or the player using CTRL-K to suicide the unit.
+
+    -- Since the weapon itself kills the unit with self.unit:Destroy(), and CTRL-K detonates the
+    -- weapon immediately thanks to selfdestruct.lua, we don't need any logic here for OnKilled()
+
+    Weapons = {        
+        Suicide = Class(CMobileKamikazeBombWeapon) {},
+    },
+    
+    OnKilled = function(self, instigator, type, overkillRatio)
+        self.FxDeath = EffectTemplate.CMobileKamikazeBombExplosion --adding the weapon explosion to death.
+    
+        local army = self:GetArmy() --if its weapon goes off it overlays twice. oh well. it doesnt look retarded anyway.
+        for k, v in self.FxDeath do
+            CreateEmitterAtBone(self,-2,army,v)
         end
+        
+        CWalkingLandUnit.OnKilled(self, instigator, type, overkillRatio)
+    end,
+    
+
+    -- Allow the trigger button to blow the weapon, resulting in OnKilled instigator 'nil'
+    OnProductionPaused = function(self)
+        self:GetWeaponByLabel('Suicide'):FireWeapon()
+    end,
 }
+
 TypeClass = XRL0302
