5e74663b7e09dd36bdad4c67b370902d106e915e
The-Balthazar <the.balthazar@gmail.com>
2016-09-12 18:04:01 +0100

    Donating Water Guard to the cause

---
+++ hook/lua/system/Blueprints.lua
@@ -0,0 +1,31 @@
+--------------------------------------------------------------------------------
+-- Hook File: /lua/system/blueprints.lua
+--------------------------------------------------------------------------------
+do
+
+local OldModBlueprints = ModBlueprints
+
+function ModBlueprints(all_blueprints)         
+    OldModBlueprints(all_blueprints)
+    
+    WaterGuard(all_blueprints.Unit)
+end
+
+--------------------------------------------------------------------------------
+-- Define splashy above water weapons as normalabovewater                                                                                
+-- Modded By: Balthazar
+--------------------------------------------------------------------------------
+
+function WaterGuard(all_bps)
+    for id, bp in all_bps do
+        if table.find(bp.Categories, 'SELECTABLE') and bp.Weapon then
+            for i, weap in bp.Weapon do
+                if weap.AboveWaterTargetsOnly and weap.DamageRadius and weap.DamageRadius > 1 and weap.DamageType == 'Normal' then
+                    weap.DamageType = 'NormalAboveWater'
+                end 
+            end 
+        end
+    end
+end
+
+end
