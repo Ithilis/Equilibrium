6b98eb3583122c806492cb6e29ed403327ea288c
Exotic-Retard <ddoogedoge@gmail.com>
2016-10-25 10:23:30 +0100

    Update selen cloak code
    now new patch doesnt break it, and it works like this:
    -when not moving its stealthed.
    -when toggle is on and not moving its weapon is disabled and it gets low
    selection priority as well.

--- hook/units/xsl0101/XSL0101_Script.lua
+++ hook/units/xsl0101/XSL0101_Script.lua
@@ -1,45 +1,80 @@
-#Sera Selen
+-----------------------------------------------------------------
+-- File     :  /cdimage/units/XSL0101/XSL0101_script.lua
+-- Summary  :  Seraphim Land Scout Script
+-- Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
+-----------------------------------------------------------------
 
-local oldXSL0101 = XSL0101
-XSL0101 = Class(oldXSL0101) {
+local SWalkingLandUnit = import('/lua/seraphimunits.lua').SWalkingLandUnit
+local SDFPhasicAutoGunWeapon = import('/lua/seraphimweapons.lua').SDFPhasicAutoGunWeapon
 
-    InvisState = State() {
-        Main = function(self)
-            self.Cloaked = false
-            local bp = self:GetBlueprint()
-            if bp.Intel.StealthWaitTime then
-                WaitSeconds( bp.Intel.StealthWaitTime )
-            end
-			self:EnableUnitIntel('RadarStealth')
-			self:EnableUnitIntel('Cloak')
-			self:SetWeaponEnabledByLabel('LaserTurret', false)	--add Ithilis
-			self.Cloaked = true
-        end,
-        
-        OnMotionHorzEventChange = function(self, new, old)
-            if new != 'Stopped' then
-                ChangeState( self, self.VisibleState )
-            end
-            SWalkingLandUnit.OnMotionHorzEventChange(self, new, old)
-        end,
+XSL0101 = Class(SWalkingLandUnit) {
+    Weapons = {
+		LaserTurret = Class(SDFPhasicAutoGunWeapon) {},
     },
-    
-    VisibleState = State() {
-        Main = function(self)
-            if self.Cloaked then
-                self:DisableUnitIntel('RadarStealth')
-			    self:DisableUnitIntel('Cloak')
-				self:SetWeaponEnabledByLabel('LaserTurret', true)	--add Ithilis
-			end
-        end,
-        
-        OnMotionHorzEventChange = function(self, new, old)
-            if new == 'Stopped' then
-                ChangeState( self, self.InvisState )
-            end
-            SWalkingLandUnit.OnMotionHorzEventChange(self, new, old)
-        end,
-    },
-	
+
+    -- Toggle disabled
+    OnScriptBitSet = function(self, bit)
+        if bit == 8 then
+            self.Sync.LowPriority = false
+        else
+            SWalkingLandUnit.OnScriptBitSet(self, bit)
+        end
+    end,
+
+    -- Toggle enabled
+    OnScriptBitClear = function(self, bit)
+        if bit == 8 then
+            self.Sync.LowPriority = true
+        else
+            SWalkingLandUnit.OnScriptBitClear(self, bit)
+        end
+    end,
+
+    RevealUnit = function(self)
+        if self.CloakThread then
+            KillThread(self.CloakThread)
+            self.CloakThread = nil
+        end
+
+        self:SetFireState(0)
+        self:DisableUnitIntel('ToggleBit5', 'RadarStealth')
+        self:DisableUnitIntel('ToggleBit8', 'Cloak')
+    end,
+
+    -- Turn off the cloak to begin with
+    OnStopBeingBuilt = function(self, builder, layer)
+        SWalkingLandUnit.OnStopBeingBuilt(self, builder, layer)
+        self:SetScriptBit('RULEUTC_CloakToggle', true)
+    end,
+
+    OnMotionHorzEventChange = function(self, new, old)
+        -- If we stopped moving, hide
+        if new == 'Stopped' then
+            -- We need to fork in order to use WaitSeconds
+            self.CloakThread = ForkThread(function()
+                WaitSeconds(2) --why is this 2 seconds?
+
+                if not self.Dead then
+                    self:EnableUnitIntel('ToggleBit5', 'RadarStealth')
+                    self:EnableUnitIntel('ToggleBit8', 'Cloak')
+                    self.CloakThread = nil
+                end
+            end)
+        end
+            
+        --if your toggle is on then we activate low priority mode and deselect and hold fire selen as well.
+        if self.Sync.LowPriority then
+            self:SetFireState(1)
+        end
+
+        -- If we begin moving, reveal ourselves
+        if old == 'Stopped' then
+            self:RevealUnit()
+        end
+
+
+        SWalkingLandUnit.OnMotionHorzEventChange(self, new, old)
+    end,
 }
+
 TypeClass = XSL0101
