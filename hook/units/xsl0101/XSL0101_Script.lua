983725b0e859770590da89cff9a828af7797f0ec
Exotic-Retard <ddoogedoge@gmail.com>
2016-10-28 19:07:15 +0100

    Update selen stealth code with proper shooting
    now is stealths correctly while shooting.
    -when it finishes shooting it stealths afterwards.
    -if its toggle is on then it wont shoot automatically, but if you order
    it to shoot then it will shoot until the unit dies then cloak, and not
    shoot again unless ordered
    -this is epic
    
    also you can now specify the ammount of time to wait until it cloaks - 1
    second minimum i guess or it will cloak before its dont killing
    something.

--- hook/units/xsl0101/XSL0101_Script.lua
+++ hook/units/xsl0101/XSL0101_Script.lua
@@ -9,13 +9,22 @@ local SDFPhasicAutoGunWeapon = import('/lua/seraphimweapons.lua').SDFPhasicAutoG
 
 XSL0101 = Class(SWalkingLandUnit) {
     Weapons = {
-		LaserTurret = Class(SDFPhasicAutoGunWeapon) {},
+		LaserTurret = Class(SDFPhasicAutoGunWeapon) {
+            OnWeaponFired = function(self) --if we fire our weapon, we must reveal ourselves.
+                self.unit:RevealUnit()
+                --we begin the hide sequence, which is cleared if we fire again.
+                self.unit.CloakThread = self.unit:ForkThread(self.unit.CloakingThread)
+                
+                SDFPhasicAutoGunWeapon.OnWeaponFired(self)
+            end,
+        },
     },
 
     -- Toggle disabled
     OnScriptBitSet = function(self, bit)
         if bit == 8 then
             self.Sync.LowPriority = false
+            self:SetWeaponEnabledByLabel('LaserTurret', true)
         else
             SWalkingLandUnit.OnScriptBitSet(self, bit)
         end
@@ -25,6 +34,9 @@ XSL0101 = Class(SWalkingLandUnit) {
     OnScriptBitClear = function(self, bit)
         if bit == 8 then
             self.Sync.LowPriority = true
+            if not self:IsMoving() then --for the case when we toggle stationary
+                self:SetWeaponEnabledByLabel('LaserTurret', false)
+            end
         else
             SWalkingLandUnit.OnScriptBitClear(self, bit)
         end
@@ -36,45 +48,49 @@ XSL0101 = Class(SWalkingLandUnit) {
             self.CloakThread = nil
         end
 
-        self:SetFireState(0)
+        self:SetWeaponEnabledByLabel('LaserTurret', true)
         self:DisableUnitIntel('ToggleBit5', 'RadarStealth')
         self:DisableUnitIntel('ToggleBit8', 'Cloak')
+    end,
+    
+    HideUnit = function(self) --this is its own function so we can call it without any delay if we want.
+        if not self.Dead then
+            self:EnableUnitIntel('ToggleBit5', 'RadarStealth')
+            self:EnableUnitIntel('ToggleBit8', 'Cloak')
+            self.CloakThread = nil
+            --if your toggle is on then we activate low priority mode and deselect and hold fire selen as well.
+            if self.Sync.LowPriority then
+                self:SetWeaponEnabledByLabel('LaserTurret', false)
+            end
+        end
     end,
 
     -- Turn off the cloak to begin with
     OnStopBeingBuilt = function(self, builder, layer)
         SWalkingLandUnit.OnStopBeingBuilt(self, builder, layer)
         self:SetScriptBit('RULEUTC_CloakToggle', true)
+        self:HideUnit() --if we are spawned in we want to hide our unit.
     end,
 
     OnMotionHorzEventChange = function(self, new, old)
         -- If we stopped moving, hide
         if new == 'Stopped' then
             -- We need to fork in order to use WaitSeconds
-            self.CloakThread = ForkThread(function()
-                WaitSeconds(2) --why is this 2 seconds?
-
-                if not self.Dead then
-                    self:EnableUnitIntel('ToggleBit5', 'RadarStealth')
-                    self:EnableUnitIntel('ToggleBit8', 'Cloak')
-                    self.CloakThread = nil
-                end
-            end)
+            self.CloakThread = self:ForkThread(self.CloakingThread)
         end
-            
-        --if your toggle is on then we activate low priority mode and deselect and hold fire selen as well.
-        if self.Sync.LowPriority then
-            self:SetFireState(1)
-        end
-
         -- If we begin moving, reveal ourselves
         if old == 'Stopped' then
             self:RevealUnit()
         end
-
-
         SWalkingLandUnit.OnMotionHorzEventChange(self, new, old)
     end,
+    
+    CloakingThread = function(self)
+        WaitSeconds(self:GetBlueprint().Intel.StealthWaitTime)
+        --we wait for specified seconds then hide our unit
+        self:HideUnit()
+    end,
+    
 }
 
 TypeClass = XSL0101
