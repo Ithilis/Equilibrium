795662fc349e170854dc65e6b44225674cbc112a
Exotic-Retard <ddoogedoge@gmail.com>
2017-01-24 12:53:10 +0000

    Hook the Cybran T3 Gunship
    it doesnt need to be shadowed to disable jamming on it, this is just a
    great waste of space.

--- balance/units/xra0305/XRA0305_script.lua
+++ balance/units/xra0305/XRA0305_script.lua
@@ -7,20 +7,12 @@
 --**
 --**  Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
 --****************************************************************************
-local CAirUnit = import('/lua/cybranunits.lua').CAirUnit
-local CAAMissileNaniteWeapon = import('/lua/cybranweapons.lua').CAAMissileNaniteWeapon
-local CDFLaserDisintegratorWeapon = import('/lua/cybranweapons.lua').CDFLaserDisintegratorWeapon02
 
-XRA0305 = Class(CAirUnit) {
-    
-    Weapons = {
-        Missiles1 = Class(CAAMissileNaniteWeapon) {},
-        Disintegrator01 = Class(CDFLaserDisintegratorWeapon) {},
-        Disintegrator02 = Class(CDFLaserDisintegratorWeapon) {},
-    },
-    
+oldCAirUnit = CAirUnit
+
+XRA0305 = Class(oldCAirUnit) {
     OnStopBeingBuilt = function(self,builder,layer)
-        CAirUnit.OnStopBeingBuilt(self,builder,layer)
+        oldCAirUnit.OnStopBeingBuilt(self,builder,layer)
         self:SetMaintenanceConsumptionInactive()
         self:SetScriptBit('RULEUTC_StealthToggle', true)
         self:RequestRefreshUI()
