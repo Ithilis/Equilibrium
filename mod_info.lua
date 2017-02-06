f1618cf75e512add231c02ec1655d3148c5ba100
Exotic-Retard <ddoogedoge@gmail.com>
2017-02-06 19:51:15 +0000

    Update mod info version
    last patch we saw the refuel feature and some small bugfixes.

--- mod_info.lua
+++ mod_info.lua
@@ -1,6 +1,6 @@
 name = "Equilibrium_Develop"
-uid = "DEVELOP1-V41R-N478-N0LK-EQBALANCEMOD"
-version = 41
+uid = "DEVELOP1-V42D-N478-N0LK-EQBALANCEMOD"
+version = 42
 copyright = "Ithilis - feel free to use this code, but ask permission first, and credit Equilibrium in your mod."
 description = "This mod fixes all balance issues with the game, and improves the gameplay to a higher level."
 icon = "/Equilibrium_balance_mod.png"
