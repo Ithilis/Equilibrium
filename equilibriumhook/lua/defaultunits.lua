2d83b38aed936f2dba461c1d1092db5671add2e6
Exotic-Retard <ddoogedoge@gmail.com>
2017-02-08 14:13:59 +0000

    Clean up defaultunits.lua
    theres some future transport changes coming but i thought it would make
    sense to do some cleaning now

--- equilibriumhook/lua/defaultunits.lua
+++ equilibriumhook/lua/defaultunits.lua
@@ -8,87 +8,16 @@ local GetRandomFloat = import('utilities.lua').GetRandomFloat
 
 --veterancy system added
 --auto refuel toggle button added
-AirUnit = Class(MobileUnit) {
+oldAirUnit = AirUnit
 
-    -- Contrails
-    ContrailEffects = {'/effects/emitters/contrail_polytrail_01_emit.bp',},
-    BeamExhaustCruise = '/effects/emitters/air_move_trail_beam_03_emit.bp',
-    BeamExhaustIdle = '/effects/emitters/air_idle_trail_beam_01_emit.bp',
-
-    -- DESTRUCTION PARAMS
-    ShowUnitDestructionDebris = false,
-    DestructionExplosionWaitDelayMax = 0,
-    DestroyNoFallRandomChance = 0.5,
+AirUnit = Class(oldAirUnit) {
 
     OnCreate = function(self)
-        MobileUnit.OnCreate(self)
-        self.HasFuel = true
+        oldAirUnit.OnCreate(self)
         self:SetAutoRefuel(true) --on by default
         --TODO: make this only work for units that have the dock order available.
-        self:AddPingPong()
     end,
-
-    AddPingPong = function(self)
-        local bp = self:GetBlueprint()
-        if bp.Display.PingPongScroller then
-            bp = bp.Display.PingPongScroller
-            if bp.Ping1 and bp.Ping1Speed and bp.Pong1 and bp.Pong1Speed and bp.Ping2 and bp.Ping2Speed
-                and bp.Pong2 and bp.Pong2Speed then
-                self:AddPingPongScroller(bp.Ping1, bp.Ping1Speed, bp.Pong1, bp.Pong1Speed,
-                                         bp.Ping2, bp.Ping2Speed, bp.Pong2, bp.Pong2Speed)
-            end
-        end
-    end,
-
-    OnMotionVertEventChange = function( self, new, old )
-        MobileUnit.OnMotionVertEventChange( self, new, old )
-        --LOG( 'OnMotionVertEventChange, new = ', new, ', old = ', old )
-        local army = self:GetArmy()
-        if (new == 'Down') then
-            -- Turn off the ambient hover sound
-            self:StopUnitAmbientSound( 'ActiveLoop' )
-        elseif (new == 'Bottom') then
-            -- While landed, planes can only see half as far
-            local vis = self:GetBlueprint().Intel.VisionRadius / 2
-            self:SetIntelRadius('Vision', vis)
-
-            -- Turn off the ambient hover sound
-            -- It will probably already be off, but there are some odd cases that
-            -- make this a good idea to include here as well.
-            self:StopUnitAmbientSound( 'ActiveLoop' )
-        elseif (new == 'Up' or ( new == 'Top' and ( old == 'Down' or old == 'Bottom' ))) then
-            -- Set the vision radius back to default
-            local bpVision = self:GetBlueprint().Intel.VisionRadius
-            if bpVision then
-                self:SetIntelRadius('Vision', bpVision)
-            else
-                self:SetIntelRadius('Vision', 0)
-            end
-        end
-    end,
-
-    OnStartRefueling = function(self)
-        self:PlayUnitSound('Refueling')
-    end,
-
-    OnRunOutOfFuel = function(self)
-        self.HasFuel = false
-        self:DestroyTopSpeedEffects()
-
-        -- penalize movement for running out of fuel
-        self:SetSpeedMult(0.35)     -- change the speed of the unit by this mult
-        self:SetAccMult(0.25)       -- change the acceleration of the unit by this mult
-        self:SetTurnMult(0.25)      -- change the turn ability of the unit by this mult
-    end,
-
-    OnGotFuel = function(self)
-        self.HasFuel = true
-        -- revert these values to the blueprint values
-        self:SetSpeedMult(1)
-        self:SetAccMult(1)
-        self:SetTurnMult(1)
-    end,
-
+    
     OnImpact = function(self, with, other)
         if self.DeathBounce then
             return
@@ -317,48 +246,12 @@ AirStagingPlatformUnit = Class(oldAirStagingPlatformUnit) {
 --- Mixin transports (air, sea, space, whatever). Sellotape onto concrete transport base classes as
 -- desired.
 
+oldBaseTransport = BaseTransport
 
-
-BaseTransport = Class() {
-    OnTransportAttach = function(self, attachBone, unit)
-        self:PlayUnitSound('Load')
-        self:RequestRefreshUI()
-
-        for i=1, self:GetBoneCount() do
-            if self:GetBoneName(i) == attachBone then
-                self.slots[i] = unit
-                unit.attachmentBone = i
-            end
-        end
-        
-        unit:OnAttachedToTransport(self, attachBone)
-    end,
-
-    OnTransportDetach = function(self, attachBone, unit)
-        self:PlayUnitSound('Unload')
-        self:RequestRefreshUI()
-        self.slots[unit.attachmentBone] = nil
-        unit.attachmentBone = nil
-        unit:OnDetachedFromTransport(self, attachBone)
-    end,
-
+BaseTransport = Class(oldBaseTransport) {
     -- When one of our attached units gets killed, detach it
     OnAttachedKilled = function(self, attached)
         attached:DetachFrom()
-    end,
-
-    OnStartTransportLoading = function(self)
-        -- We keep the aibrain up to date with the last transport to start loading so, among other
-        -- things, we can determine which transport is being referenced during an OnTransportFull
-        -- event (As this function is called immediately before that one).
-        self.transData = {}
-        self:GetAIBrain().loadingTransport = self
-    end,
-
-    OnStopTransportLoading = function(...)
-    end,
-
-    DestroyedOnTransport = function(self)
     end,
 
     DetachCargo = function(self)
