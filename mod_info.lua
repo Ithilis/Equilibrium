bfccd92cca51c5c1c1a39b8f6667b150501dd0d1
Exotic-Retard <ddoogedoge@gmail.com>
2017-02-23 17:43:51 +0000

    Update mod info version
    yeah needed a hotfix in there. damn.

--- mod_info.lua
+++ mod_info.lua
@@ -1,6 +1,6 @@
 name = "Equilibrium_Develop"
-uid = "DEVELOP1-V43D-3980-OSCK-EQBALANCEMOD"
-version = 43
+uid = "DEVELOP1-V44D-H3KM-D0EN-EQBALANCEMOD"
+version = 44
 copyright = "Ithilis - feel free to use this code, but ask permission first, and credit Equilibrium in your mod."
 description = "This mod fixes all balance issues with the game, and improves the gameplay to a higher level."
 icon = "/Equilibrium_balance_mod.png"
