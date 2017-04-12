91f4ed6a11949adcc21653a644bd514e790d56ab
Exotic-Retard <ddoogedoge@gmail.com>
2017-04-12 20:26:23 +0100

    Improve stun mechanics
    now the ignore air flag is no longer hard coded, and it uses a cylinder
    with a capped height instead of a spehere which removes issues at the
    edges of its range in height differences.

--- equilibriumhook/lua/sim/Unit.lua
+++ equilibriumhook/lua/sim/Unit.lua
@@ -582,4 +582,55 @@ Unit = Class(oldUnit) {
 		end
     end,
 
+-------------------------------------------------------------------------------------------
+-- BUFFS
+-------------------------------------------------------------------------------------------
+--there was some fucked up changes to do with stun mechanics so we need to hook these to fix them
+--no hardcoding to exclude air units please
+--spheres make 500 times less sense than capped cylinders in supcom. ever heard of terrain?
+    AddBuff = function(self, buffTable, PosEntity)
+        local bt = buffTable.BuffType
+        if not bt then
+            error('*ERROR: Tried to add a unit buff in unit.lua but got no buff table.  Wierd.', 1)
+            return
+        end
+
+        -- When adding debuffs we have to make sure that we check for permissions
+        local category = buffTable.TargetAllow and ParseEntityCategory(buffTable.TargetAllow) or categories.ALLUNITS
+        if buffTable.TargetDisallow then
+            category = category - ParseEntityCategory(buffTable.TargetDisallow)
+        end
+
+        if bt == 'STUN' then
+            local targets
+            if buffTable.Radius and buffTable.Radius > 0 then
+                -- If the radius is bigger than 0 then we will use the unit as the center of the stun blast
+                targets = utilities.GetTrueEnemyUnitsInCylinder(self, PosEntity or self:GetPosition(), buffTable.Radius, buffTable.Height, category)
+            else
+                -- The buff will be applied to the unit only
+                if EntityCategoryContains(category, self) then
+                    targets = {self}
+                end
+            end
+            for _, target in targets or {} do
+                -- Exclude things currently flying around if we have a flag
+                if not (buffTable.ExcludeAirLayer and target:GetCurrentLayer() == 'Air') then
+                    target:SetStunned(buffTable.Duration or 1)
+                end
+            end
+        elseif bt == 'MAXHEALTH' then
+            self:SetMaxHealth(self:GetMaxHealth() + (buffTable.Value or 0))
+        elseif bt == 'HEALTH' then
+            self:SetHealth(self, self:GetHealth() + (buffTable.Value or 0))
+        elseif bt == 'SPEEDMULT' then
+            self:SetSpeedMult(buffTable.Value or 0)
+        elseif bt == 'MAXFUEL' then
+            self:SetFuelUseTime(buffTable.Value or 0)
+        elseif bt == 'FUELRATIO' then
+            self:SetFuelRatio(buffTable.Value or 0)
+        elseif bt == 'HEALTHREGENRATE' then
+            self:SetRegenRate(buffTable.Value or 0)
+        end
+    end,
+    
 }
