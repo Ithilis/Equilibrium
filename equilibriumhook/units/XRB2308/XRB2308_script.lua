80713c084e32dd3f1b99273f398bd9c72c435d0d
Exotic-Retard <ddoogedoge@gmail.com>
2017-05-28 10:44:19 +0100

    Update HARMS script
    now that the code is also in base faf we only need to hook a little bit
    of it to get it to work how we want. this also makes it less needed to
    be updated manually if any changes are made on the faf end

--- equilibriumhook/units/XRB2308/XRB2308_script.lua
+++ equilibriumhook/units/XRB2308/XRB2308_script.lua
@@ -10,112 +10,33 @@
 local CStructureUnit = import('/lua/cybranunits.lua').CStructureUnit
 local CKrilTorpedoLauncherWeapon = import('/lua/cybranweapons.lua').CKrilTorpedoLauncherWeapon
 
-XRB2308 = Class(CStructureUnit) {
-    Weapons = {
-        Turret01 = Class(CKrilTorpedoLauncherWeapon) {},
-    },
-    
-    OnStopBeingBuilt = function(self,builder,layer)
-        CStructureUnit.OnStopBeingBuilt(self,builder,layer)
-        self:StartSinking()
-        
-        local army = self:GetArmy() --add inital sinking effects
-        self.Trash:Add(CreateAttachedEmitter(self,'xrb2308', army, '/effects/emitters/tt_water02_footfall01_01_emit.bp'):ScaleEmitter(1.4)) --one-off
-        self.Trash:Add(CreateAttachedEmitter(self,'xrb2308', army, '/effects/emitters/tt_snowy01_landing01_01_emit.bp'):ScaleEmitter(1.5)) --one-off
+local oldXRB2308 = XRB2308
+XRB2308 = Class(oldXRB2308) {
+    -- Sink to the bottom, or to a pre-set depth
+    DepthWatcher = function(self)
+        self.sinkingFromBuild = true
 
-        ChangeState(self, self.IdleState)
-    end,
-    
-    StartSinking = function(self)
-        local bone = 0
-        
-        local army = self:GetArmy() --add sinking effect for the duration of the sinking
-        self.Trash:Add(CreateAttachedEmitter(self,'xrb2308', army, '/effects/emitters/tt_water_submerge02_01_emit.bp'):ScaleEmitter(1.5)) --continuous
-        --Create sinker projectile
-        local proj = self:CreateProjectileAtBone('/projectiles/Sinker/Sinker_proj.bp', bone)
-        self.sinkProjectile = proj
-        -- Start the sinking after a delay of the given number of seconds, attaching to a given bone
-        -- and entity.
-        proj:SetVelocityAlign(false)
-        proj:SetLocalAngularVelocity(0, 0, 0) -- change this to make it rotate some while sinking
-        proj:Start(0, self, bone, self.CheckDepth)
-        proj:SetBallisticAcceleration(-0.75)
-        self.Trash:Add(proj)
-        self.Depthwatcher = self:ForkThread(self.SinkDepthThread)
-    end,
-    
-    CheckDepth = function(self)
-    return
-    end,
-    
-    SinkDepthThread = function(self)
-        --self:SeabedWatcher()-- Waits for wreck to hit bottom or end of animation
-        WaitSeconds(5.2) --use this to set the depth - basic maths required (:
-        
-        if not self:IsDead() and self.sinkProjectile then
-            self.sinkProjectile:Destroy()
-            self.sinkProjectile = nil
+        local sinkFor = 5.2 -- Use this to set the depth - Basic maths required - EQ: we increase this to a sane amount
+        while self.sinkProjectile and sinkFor > 0 do
+            WaitTicks(1)
+            sinkFor = sinkFor - 0.1
+        end
+
+        local bottom = true
+        if not self.Dead then
+            if self.sinkProjectile then
+                bottom = false -- We must have timed out
+                self.sinkProjectile:Destroy()
+                self.sinkProjectile = nil
+            end
+
+            -- Stop the unit's momentum
             self:SetPosition(self:GetPosition(), true)
             self:FinalAnimation()
         end
+
+        self.sinkingFromBuild = false
+        self.Bottom = bottom
     end,
-    
-    FinalAnimation = function(self)
-        if self.sinkProjectile then --clearing the sink projectile in case we need to reuse it.
-            self.sinkProjectile:Destroy()
-            self.sinkProjectile = nil
-        elseif self:IsDead() then return end
-        
-        --setting the deploy animation to the end of its sinking and not the start.
-        local bp = self:GetBlueprint()
-        local bpAnim = bp.Display.AnimationDeploy
-        if not bpAnim then return end
-        if not self.OpenAnim then
-            self.OpenAnim = CreateAnimator(self)
-            self.OpenAnim:PlayAnim(bpAnim)
-            self.Trash:Add(self.OpenAnim)
-        end
-        
-        self:PlaySound(bp.Audio.Deploy)
-    end,
-    
-    --hijacking the original death thread so it doesnt disrupt our sinking.
-    DeathThread = function( self, overkillRatio, instigator)
-        self:DestroyTopSpeedEffects()
-        self:DestroyIdleEffects()
-        self:DestroyBeamExhaust()
-        self:DestroyAllBuildEffects()
-        
-        local bp = self:GetBlueprint()
-        self:PlaySound(bp.Audio.Destroyed)
-        local army = self:GetArmy() --add an initial death explosion
-        self.Trash:Add(CreateAttachedEmitter(self,'xrb2308', army, '/effects/emitters/flash_03_emit.bp'):ScaleEmitter(2))
-        self.Trash:Add(CreateAttachedEmitter(self,'xrb2308', army, '/effects/emitters/flash_04_emit.bp'):ScaleEmitter(2))
-        
-        if self.ShowUnitDestructionDebris and overkillRatio then -- Flying bits of metal and whatnot. More bits for more overkill.
-            self.CreateUnitDestructionDebris(self, true, true, overkillRatio > 2)
-        end
-        
-        self.DisallowCollisions = true
-        self:ForkThread(self.SinkDestructionEffects) -- Bubbles and stuff coming off the sinking wreck.
-        self.overkillRatio = overkillRatio -- Avoid slightly ugly need to propagate this through callback hell...
-        
-        if not self.sinkProjectile then --in the case that we are not on the seabed
-            self:StartSinking()
-        end
-        
-        self:SeabedWatcher()-- Waits for wreck to hit bottom or end of animation
-        self:DestroyAllDamageEffects()
-        
-        
-        if self.PlayDestructionEffects then --moved to the end to make sure its sunk before exploding
-            self:CreateDestructionEffects(overkillRatio)
-        end
-        
-        self:PlayUnitSound('Destroyed')
-        self:CreateWreckage(overkillRatio, instigator)
-        self:Destroy()
-    end,
-    
 }
 TypeClass = XRB2308
