5ad6823113699b093dfcc786733c748067f0ed6e
Exotic-Retard <ddoogedoge@gmail.com>
2017-06-12 20:20:33 +0100

    Update mod info
    new version time! this ones primary purpose is bugfixing but also
    contains a couple of important balance changes like mass storage cost

--- mod_info.lua
+++ mod_info.lua
@@ -1,6 +1,6 @@
 name = "Equilibrium_Balance_Mod"
-uid = "FEATURE1-V45F-K539-39N3-EQBALANCEMOD"
-version = 45
+uid = "FEATURE1-V46F-MED6-VU59-EQBALANCEMOD"
+version = 46
 copyright = "Ithilis - feel free to use this code, but ask permission first, and credit Equilibrium in your mod."
 description = "This mod fixes all balance issues with the game, and improves the gameplay to a higher level."
 icon = "/Equilibrium_balance_mod.png"
