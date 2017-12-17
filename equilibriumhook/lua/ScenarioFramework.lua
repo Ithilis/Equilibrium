8018018600ecd28aa0c23d42792e89125bb268be
Exotic-Retard <ddoogedoge@gmail.com>
2017-12-17 21:59:00 +0000

    Improve SeabedReveal performance
    way fewer threads needed. fantastic.
    
    this current state could still be improved though. working on that.

---
+++ equilibriumhook/lua/ScenarioFramework.lua
@@ -0,0 +1,40 @@
+-----------------------------------------------------------------
+-- File       : /lua/scenarioFramework.lua
+-- Authors    : John Comes, Drew Staltman
+-- Summary    : Functions for use in the scenario scripts.
+-- Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+-----------------------------------------------------------------
+
+--EQ:we end up using a system where each unit gets a timestamp, and this thread checks it,
+--and removes the watervision effects of those that have expired.
+WaterVisionResetThread = function(self)
+	--WARN('thread starting')
+    WaitTicks(15) --initial delay
+
+    while ScenarioInfo.SeabedRevealThreadAllowed == true do
+		local neededUnits = {}
+		local tick = GetGameTick() --for some reason the timer ends a tick early but w.e.
+		local units = GetUnitsInRect(Rect(unpack(ScenarioInfo.PlayableArea))) --get all units in the playable area. yeah.
+		
+		--to avoid a complete mess, we filter the unit table and work out which units we need to touch.
+		for k, unit in units do
+			if not unit:IsDead() then
+				if unit.WVizEndTick ~= false and unit.WVizEndTick <= tick then
+					unit.WVizEndTick = false --flag this unit as having been worked on
+					table.insert(neededUnits, unit)
+				end
+			end
+		end
+		
+		for k, unit in neededUnits do
+			--normally this should be checked for IsDead, but we already checked during the filtering.
+			if unit.NormalWVision and unit.NormalWVision ~= false then
+				--WARN('reseting wvision radius')
+				unit:SetIntelRadius('watervision', unit.NormalWVision)
+				unit.NormalWVision = false --reset the variable so we can change it again if needed later
+			end
+		end
+		--WARN(string.format('%s Units checked. %s Units Reset. GameTick: %s', table.getsize(units), table.getsize(neededUnits), tick))
+		WaitTicks(11) --prepare to do the same thing next tick. 11 because waitticks waits for ticks-1 for some reason.
+	end
+end
--- lua/SeabedReveal.lua
+++ lua/SeabedReveal.lua
@@ -27,50 +27,22 @@ function SeabedReveal(SuperClass)
             end
         end,
         
-        ResetVisionThread = function(self, units)
-            WaitSeconds(8)
-            --WARN('thread ending')
-            local neededUnits = {}
-            local tick = GetGameTick() + 1 --for some reason the timer ends a tick early but w.e.
-            
-            --to avoid a complete mess, we filter the unit table and work out which units we need to touch.
-            --we want to touch only those that havent already been done in the same tick, and wont be touched in a future tick either.
-            --that way, units are only worked on by one thread, and not looked at multiple times.
-            for k, unit in units do
-                if not unit:IsDead() then
-                    if unit.WVizEndTick ~= false and unit.WVizEndTick <= tick then
-                        unit.WVizEndTick = false --flag this unit as having been worked on, so threads in the same tick dont try it.
-                        table.insert(neededUnits, unit)
-                    end
-                end
-            end
-            
-            for k, unit in neededUnits do
-                --normally this should be checked for IsDead, but we already checked during the filtering.
-                if unit.NormalWVision and unit.NormalWVision ~= false then
-                    --WARN('reseting wvision radius')
-                    unit:SetIntelRadius('watervision', unit.NormalWVision)
-                    unit.NormalWVision = false --reset the variable so we can change it again if needed later
-                end
-            end
-        end,
-        
         CreateProjectileAtMuzzle = function(self, muzzle)
             SuperClass.CreateProjectileAtMuzzle(self, muzzle)
-            self:RevealUnit()
+            self:FlagReveal()
         end,
         
-        RevealUnit = function(self)
+        FlagReveal = function(self) --this reveals ourselves to surrounding units and flags them for a global thread to deactivate later.
             if self.unit.ShouldRevealSeabed == true then
                 if self.RevealSeabed == true then
                     --WARN('flagging reveal')
                     --here we find all the units in range of ours, that could be affected, and give them watervision radius equal to their visionradius for a time.
                     local aiBrain = self.unit:GetAIBrain()
                     local units = aiBrain:GetUnitsAroundPoint(categories.STRUCTURE + categories.MOBILE, self.unit:GetPosition(), self.RevealRadius, 'Enemy')
-                    local tick = GetGameTick()
+                    local tick = GetGameTick() + 1 --for some reason the timer ends a tick early but w.e.
                     
                     for k, unit in units do
-                        unit.WVizEndTick = tick + 80 --we need to have some fancy timers since we want to reset the duration, and for this to work across multiple threads
+                        unit.WVizEndTick = tick + 80 --we need to have some fancy timers since we want to reset the duration, and for this to transcend any threading
                         
                         --only mess with the units that we havent touched before
                         if not unit.NormalWVision or unit.NormalWVision == false then
@@ -80,10 +52,9 @@ function SeabedReveal(SuperClass)
                         end
                     end
                     
-                    self:ForkThread(self.ResetVisionThread, units)
                 end
                 
-                --unlike the ResetVisionThread, we only want one of these to exist at a time. this stops us from spamming the visionthreads too much.
+                --this stops us from spamming the vision changes too much.
                 if not self.TimerThread then
                     --WARN('starting cooldownthread')
                     self.TimerThread = self:ForkThread(self.CooldownThread)
