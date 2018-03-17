f9327933acdaf82cd29a735a33243e3159233ebc
Exotic-Retard <ddoogedoge@gmail.com>
2018-03-17 11:29:21 +0000

    Optimise SeabedReveal
    this was cycling through two tables, now it cycles through only one.
    also in case a unit has a nil value on its watervision and has an
    expired timestamp, we reset it to the blueprint value or our best guess
    for it, so they have no chance to clog up the table. not that it should
    happen in any case.

--- equilibriumhook/lua/ScenarioFramework.lua
+++ equilibriumhook/lua/ScenarioFramework.lua
@@ -15,34 +15,32 @@ WaterVisionResetThread = function(self)
     WaitTicks(15) --initial delay
 
     while true do
-        local neededUnits = {}
         local tick = GetGameTick() --for some reason the timer ends a tick early but w.e.
         local units = SeabedRevealingUnits --get all units in our table
         
         --to avoid a complete mess, we filter the unit table and work out which units we need to touch.
         for key, unit in units do
             if not unit.Dead then
-                if unit.WVizEndTick ~= false and unit.WVizEndTick <= tick then
+                if unit.WVizEndTick and unit.WVizEndTick <= tick then
                     unit.WVizEndTick = nil --clean the flag up when its not in use
-                    table.insert(neededUnits, unit)
+                    if unit.NormalWVision then
+                        --WARN('reseting wvision radius')
+                        unit:SetIntelRadius('watervision', unit.NormalWVision)
+                        unit.NormalWVision = nil --reset the variable so we can change it again if needed later
+                    else
+                        WARN('EQ:Unit is not dead and has expired watervision timestamp but no NormalWVision! Attempting to set to Blueprint value, then removing from table!')
+                        WARN(unit.NormalWVision)
+                        local bpValue = unit:GetBlueprint().Intel.WaterVisionRadius or 15
+                        unit:SetIntelRadius('watervision', bpValue)
+                    end
+                    table.remove(SeabedRevealingUnits, key) --remove units with expired timers from our table
                 end
             else
-            table.remove(SeabedRevealingUnits, key) --remove the units from our table
+                table.remove(SeabedRevealingUnits, key) --remove dead units from our table
             end
         end
         
-        for key, unit in neededUnits do
-            --normally this should be checked for IsDead, but we already checked during the filtering.
-            if unit.NormalWVision then
-                --WARN('reseting wvision radius')
-                unit:SetIntelRadius('watervision', unit.NormalWVision)
-                unit.NormalWVision = nil --reset the variable so we can change it again if needed later
-                table.remove(SeabedRevealingUnits, key) --remove the units from our table
-            else
-            WARN('EQ:Unit is not dead and has expired watervision timestamp but no NormalWVision! not removing from table!')
-            end
-        end
-        --WARN(string.format('%s Units checked. %s Units Reset. GameTick: %s', table.getsize(units), table.getsize(neededUnits), tick))
+        --WARN(string.format('%s Units checked. GameTick: %s', table.getsize(units), tick))
         WaitTicks(11) --prepare to do the same thing next tick. 11 because waitticks waits for ticks-1 for some reason.
     end
 end
