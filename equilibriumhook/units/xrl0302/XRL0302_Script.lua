f75012c774ac432ba3e608e5325a00fa9a72d8bb
Exotic-Retard <ddoogedoge@gmail.com>
2018-04-02 00:47:10 +0100

    Overhaul Beetle Useability
    Yeah. They were always a pain in the ass to use so now thats much
    better.
    
    - explode on death so you dont have to deal with not pressing the ctrlk
    button too late
    - explode with no friendly fire so you dont have to worry about them
    doing more harm than good
    - automatically explode near high value targets. you can now micro them
    with just move orders and they will blow up next to places where it
    hurts by themselves.
    - you can turn this off by setting them to hold fire
    - as before they can be manually exploded with ctrlk or the button
    regardless of fire stance
    - as before in eq they can be cloaked for many shenanigans, and overall
    increased survivability.
    - as a bonus, they are now protected against balance team changes as
    well. so thats cool.

--- equilibriumhook/units/xrl0302/XRL0302_Script.lua
+++ equilibriumhook/units/xrl0302/XRL0302_Script.lua
@@ -1,39 +1,90 @@
--- Fire Beetle
+----------------------------------------------------------------
+-- File     :  /data/units/XRL0302/XRL0302_script.lua
+-- Author(s):  Jessica St. Croix, Gordon Duclos
+-- Summary  :  Cybran Mobile Bomb Script
+-- Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
 -----------------------------------------------------------------
 local CWalkingLandUnit = import('/lua/cybranunits.lua').CWalkingLandUnit
 local CMobileKamikazeBombWeapon = import('/lua/cybranweapons.lua').CMobileKamikazeBombWeapon
-local EffectTemplate = import('/lua/EffectTemplates.lua')
 
 XRL0302 = Class(CWalkingLandUnit) {
 
-
-    Weapons = {        
-        Suicide = Class(CMobileKamikazeBombWeapon) {},
+    Weapons = {
+        Suicide = Class(CMobileKamikazeBombWeapon) {
+            --EQ: the kamikaze weapon is kinda dumb since it doesnt accept damagemods and the like. so we have to rewrite it a little.
+            OnFire = function(self)
+                --add the effects
+                local army = self.unit:GetArmy()
+                for k, v in self.FxDeath do
+                    CreateEmitterAtBone(self.unit,-2,army,v)
+                end
+                --do the damage
+                local bp = self:GetBlueprint()
+                local damage = bp.Damage + (self.DamageMod or 0)
+                DamageArea(self.unit, self.unit:GetPosition(), bp.DamageRadius, damage, bp.DamageType or 'Normal', bp.DamageFriendly or false)
+                self.unit:PlayUnitSound('Destroyed')
+                --self.unit:ApplyStun() --EQ:add stun (left for later incase we decide this is a good idea) yes balance team i know youre reading this. i left it for you.
+                self.unit:Destroy()
+            end,
+            
+            OnGotTarget = function(self)
+                CMobileKamikazeBombWeapon.OnGotTarget(self)
+                self:ForkThread(self.TargetThread)
+            end,
+            
+            --EQ:once we are in range of something tasty we need to look out for better targets, since the weapon doesnt do that by itself before firing
+            TargetThread = function(self)
+                while self.NumTargets > 0 do
+                    WaitSeconds(0.3)
+                    if not self.Dead then
+                        self:ResetTarget()
+                    end
+                end
+            end,
+        },
     },
     
-    OnKilled = function(self, instigator, type, overkillRatio)
-        self.FxDeath = EffectTemplate.CMobileKamikazeBombExplosion --adding the weapon explosion to death.
-    
-        local army = self:GetArmy() --if its weapon goes off it overlays twice. oh well. it doesnt look retarded anyway.
-        for k, v in self.FxDeath do
-            CreateEmitterAtBone(self,-2,army,v)
-        end
-        
-        CWalkingLandUnit.OnKilled(self, instigator, type, overkillRatio)
-    end,
-    
-
-    -- Allow the trigger button to blow the weapon, resulting in OnKilled instigator 'nil'
-    OnProductionPaused = function(self)
-        self:GetWeaponByLabel('Suicide'):FireWeapon()
-    end,
-    
+    --Turn the cloak off by default
     OnStopBeingBuilt = function(self,builder,layer)
         CWalkingLandUnit.OnStopBeingBuilt(self,builder,layer)
         self:SetMaintenanceConsumptionInactive()
         self:SetScriptBit('RULEUTC_CloakToggle', true)
         self:RequestRefreshUI()
-    end
+    end,
+    
+    -- Allow the trigger button to blow the weapon, resulting in OnKilled instigator 'nil'
+    OnProductionPaused = function(self)
+        self:GetWeaponByLabel('Suicide'):FireWeapon()
+    end,
+    
+    --EQ:we reduce the damage of the explosion if it was killed without being triggered by anything (is this needed?)
+    OnKilled = function(self, instigator, type, overkillRatio)
+        if self:GetCurrentLayer() == 'Land' then --avoid them exploding in transports
+            local bp = self:GetBlueprint().Weapon[1] --EQ:this will break if a weapon is added before this one.
+            local wep = self:GetWeaponByLabel('Suicide')
+            wep:AddDamageMod(bp.DamageMod)
+            self:GetWeaponByLabel('Suicide'):FireWeapon()
+        end
+        CWalkingLandUnit.OnKilled(self, instigator, type, overkillRatio)
+    end,
+    
+    --EQ:we apply the stun manually since the weapon didnt have it defined. well whatever.
+    ApplyStun = function(self)
+        if self:IsBeingBuilt() then return end --dont want to explode when not finished
+
+        -- Find OnDeath Buffs
+        local bp
+        for k, v in self:GetBlueprint().Buffs do
+            if v.Add.OnDeath then
+                bp = v
+            end
+        end
+
+        -- Apply buffs
+        if bp ~= nil then
+            self:AddBuff(bp)
+        end
+    end,
 }
 
 TypeClass = XRL0302
