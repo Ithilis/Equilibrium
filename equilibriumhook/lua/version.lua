b333425598bf166b261a77625d9f77de2cd72e10
Exotic-Retard <ddoogedoge@gmail.com>
2018-08-02 23:39:42 +0100

    Update to EQ v58
    the last patch before i leave on holiday! better hope nothing breaks! no
    ones going to fix it if im not around!

--- equilibriumhook/lua/version.lua
+++ equilibriumhook/lua/version.lua
@@ -1,4 +1,4 @@
-local Version = 'Equilibrium 57'
+local Version = 'Equilibrium 58'
 function GetVersion()
     LOG('Supreme Commander: Forged Alliance version ' .. Version)
     return Version
--- mod_info.lua
+++ mod_info.lua
@@ -1,6 +1,6 @@
 name = "Equilibrium_Balance_Mod"
-uid = "FEATURE1-V57F-INRG-9845-EQBALANCEMOD"
-version = 57
+uid = "FEATURE1-V58F-K3I0-A2l0-EQBALANCEMOD"
+version = 58
 copyright = "Ithilis - feel free to use this code, but ask permission first, and credit Equilibrium in your mod."
 description = "This mod fixes all balance issues with the game, and improves the gameplay to a higher level."
 icon = "/Equilibrium_balance_mod.png"
